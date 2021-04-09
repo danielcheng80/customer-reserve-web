86bc497732a2811a3c540b534f302e372146cbbb
Daniel Cheng <danielcheng80@gmail.com>
2021-04-09 14:44:26 +0800

    install ant design v4 and build main layout

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import routes from "components/router/routes";
 import RouteWithSubRoutes from "components/router/RouteWithSubRoutes";
-import "./App.css";
+import "antd/dist/antd.css";
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        {routes.map((route, i) => (
-          <RouteWithSubRoutes key={i} {...route} />
+        {routes.map((route) => (
+          <RouteWithSubRoutes key={route.path} {...route} />
         ))}
       </Switch>
     </Router>
